ae1c5d1fcab8afdb595d31861123741d66fe959a
anastacia_gal <anastacia_gal@mail.ru>
2023-06-12 15:12:20 +0300

    Добавлена возможность прикрепить файл

--- src/main/java/com/sample/drivers/screen/document/DocumentEdit.java
+++ src/main/java/com/sample/drivers/screen/document/DocumentEdit.java
@@ -1,10 +1,30 @@
 package com.sample.drivers.screen.document;
 
+import io.jmix.core.DataManager;
+import io.jmix.core.SaveContext;
+import io.jmix.ui.component.FileStorageUploadField;
+import io.jmix.ui.component.SingleFileUploadField;
 import io.jmix.ui.screen.*;
 import com.sample.drivers.entity.Document;
+import io.jmix.ui.upload.TemporaryStorage;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.UUID;
 
 @UiController("Document.edit")
 @UiDescriptor("document-edit.xml")
 @EditedEntityContainer("documentDc")
 public class DocumentEdit extends StandardEditor<Document> {
+    @Autowired
+    private FileStorageUploadField uploadDriverDocument;
+    @Autowired
+    private TemporaryStorage temporaryStorage;
+    @Autowired
+    private DataManager dataManager;
+
 }
--- src/main/java/com/sample/drivers/screen/driver/DriverEdit.java
+++ src/main/java/com/sample/drivers/screen/driver/DriverEdit.java
@@ -1,8 +1,9 @@
 package com.sample.drivers.screen.driver;
 
-import io.jmix.ui.component.FileUploadField;
-import io.jmix.ui.component.HasValue;
-import io.jmix.ui.component.Image;
+import com.sample.drivers.entity.Document;
+import io.jmix.ui.UiComponents;
+import io.jmix.ui.component.*;
+import io.jmix.ui.download.Downloader;
 import io.jmix.ui.screen.*;
 import com.sample.drivers.entity.Driver;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,10 @@ public class DriverEdit extends StandardEditor<Driver> {
     private Image photoImage;
     @Autowired
     private FileUploadField photoFileUpload;
+    @Autowired
+    private UiComponents uiComponents;
+    @Autowired
+    private Downloader downloader;
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
@@ -26,6 +31,23 @@ public class DriverEdit extends StandardEditor<Driver> {
     public void onPhotoFileUploadValueChange(HasValue.ValueChangeEvent<byte[]> event) {
         photoFileUpload.getId();
         photoImage.setVisible(true);
+    }
+
+    @Install(to = "documentsTable.documentFileColumn", subject = "columnGenerator")
+    private Component documentsTableDocumentFileColumnColumnGenerator(Document document) {
+
+        if (document.getDocumentFile() != null) {
+            LinkButton linkButton = uiComponents.create(LinkButton.class);
+
+            linkButton.setCaption(document.getDocumentFile().getFileName());
+            linkButton.addClickListener(clickEvent ->
+                    downloader.download(document.getDocumentFile()));
+
+            return linkButton;
+        }
+
+
+        return null;
     }
 
 
