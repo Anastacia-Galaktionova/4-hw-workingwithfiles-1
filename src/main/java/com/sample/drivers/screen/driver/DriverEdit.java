0fa6adb21f8d28a60ff7e32bed8479663592a12f
anastacia_gal <anastacia_gal@mail.ru>
2023-06-12 11:20:16 +0300

    Реализована возможность загрузки фото (1 пункт)

--- src/main/java/com/sample/drivers/screen/driver/DriverEdit.java
+++ src/main/java/com/sample/drivers/screen/driver/DriverEdit.java
@@ -1,10 +1,32 @@
 package com.sample.drivers.screen.driver;
 
+import io.jmix.ui.component.FileUploadField;
+import io.jmix.ui.component.HasValue;
+import io.jmix.ui.component.Image;
 import io.jmix.ui.screen.*;
 import com.sample.drivers.entity.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @UiController("Driver.edit")
 @UiDescriptor("driver-edit.xml")
 @EditedEntityContainer("driverDc")
 public class DriverEdit extends StandardEditor<Driver> {
+    @Autowired
+    private Image photoImage;
+    @Autowired
+    private FileUploadField photoFileUpload;
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        photoImage.setVisible(false);
+    }
+
+    @Subscribe("photoFileUpload")
+    public void onPhotoFileUploadValueChange(HasValue.ValueChangeEvent<byte[]> event) {
+        photoFileUpload.getId();
+        photoImage.setVisible(true);
+    }
+
+
 }
